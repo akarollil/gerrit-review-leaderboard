bd3f888c60d7ecfd47be8a3863963ce60956f163
Anoop Karollil <anoop.karollil@gmail.com>
2016-02-09 19:07:20 -0800

    Add view and template to display reviewer list
    Add a view to fetch changes and to support a simple index page template
    that displays a table of reviewers, review count, and comment count.

--- django-site/leaderboard/views.py
+++ django-site/leaderboard/views.py
@@ -1,6 +1,30 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+""" View to display a simple page with tables containing information about
+gerrit reviewers
+"""
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-from django.shortcuts import render
+from .models import Reviewer
+from .sync import fetcher
 
-# Create your views here.
+HOST_NAME = "gerrit.myhost.com"
+
+
+def index(request):
+    # fetch outstanding changes, up to a configured maximum specified in
+    # sync/fetcher.conf
+    fetcher.pull_and_store_changes(HOST_NAME)
+    reviewers = Reviewer.objects.all()
+    reviewers_list = []
+    for reviewer in reviewers:
+        reviewer_name = reviewer.full_name
+        review_count = reviewer.changes.count
+        comment_count = reviewer.comments.count
+        reviewers_list.append({"name": reviewer_name,
+                               "review_count": review_count,
+                               "comment_count": comment_count})
+
+    template = loader.get_template('leaderboard/index.html')
+    context = RequestContext(request, {'reviewers': reviewers_list})
+
+    return HttpResponse(template.render(context))
